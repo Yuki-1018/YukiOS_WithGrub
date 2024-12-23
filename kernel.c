7315de59f9d00cd0c3cd6a18d38d710819f1de25
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2024-12-23 12:10:07 +0900

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,17 +1,15 @@
-// kernel.c
+kernel.c 
 #include "multiboot.h"
 
-void kernel_main() {
-    // VGAテキストモードにアクセスしてメッセージを表示
-    const char *message = "Welcome to Yuki OS Kernel!";
-    char *video_memory = (char *) 0xB8000; // VGAテキストモードのメモリ開始アドレス
+void
+main(void)
+{
+    unsigned short *screen = (unsigned short *)0xB8000;
+    char *msg = "Wecome to Yuki OS Kernel!";
+    char ch; 
     
-    // メッセージを表示する
-    for (int i = 0; message[i] != '\0'; i++) {
-        video_memory[i * 2] = message[i];       // 文字
-        video_memory[i * 2 + 1] = 0x07;         // 白色の文字
-    }
+    while((ch=*msg++))
+        *screen++ = ((0x0F)<<8) | ch;
 
-    // 無限ループ（カーネルが終了しないようにする）
-    while (1) {}
+    while(1);
 }
